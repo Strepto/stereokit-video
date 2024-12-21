765d285f3ffb7c42372cfce54f289bee90c1c427
Nick Klingensmith <8484884+maluoi@users.noreply.github.com>
2024-12-21 07:14:50 -0800

    Some reusability changes! (#1)
    * Move player UI out of the VideoKitPlayer class for re-usability.
    
    Different method of doing time seek.
    Positional audio hooked up to the video.
    
    * Make video texture a `Sprite` and display it on the UI instead of a free-floating Mesh.
    
    Video Tex uses AddressMode.Clamp by default now.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using StereoKit;
+using System;
 
 namespace VideoKit;
 
@@ -11,28 +12,45 @@ class Program
         if (!SK.Initialize(settings))
             return;
 
-        using var videoPlayer = new VideoKitPlayer();
-        var texture = videoPlayer.InitializeSoundAndTexture();
-
-        videoPlayer.PlayVideoAsync("");
-
         // Create assets used by the app
-        Pose cubePose = new Pose(0, 0, -1.5f);
-        Model cube = Model.FromMesh(Mesh.GenerateRoundedCube(new Vec3(16, 9, 1) / 10, 0.02f), Material.UI);
+
+        using VideoKitPlayer videoPlayer = new();
+        videoPlayer.PlayVideoAsync(new Uri("https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"));
+        Sprite videoSprite = Sprite.FromTex( videoPlayer.InitializeSoundAndTexture(), SpriteType.Single );
 
         Renderer.SkyTex = Tex.FromCubemap("sky/space.ktx2");
 
-        Material videoMaterial = Material.Unlit;
-        videoMaterial[MatParamName.DiffuseTex] = texture;
-
+        Pose uiPose = UI.PopupPose();
+        float timestampDisplay = 0;
         // Core application loop
         SK.Run(() =>
         {
             videoPlayer.Step();
-            UI.Handle("Cube", ref cubePose, cube.Bounds);
+            videoPlayer.SoundPosition = uiPose.position;
 
-            // videoPlayer.ActiveSoundStream.Play(cubePose.position);
-            cube.Draw(videoMaterial, cubePose.ToMatrix(), Color.White);
+            UI.WindowBegin("VideoKitPlayer", ref uiPose, new Vec2(0.4f,0));
+
+            UI.Image(videoSprite, new Vec2(UI.LayoutRemaining.x,0));
+
+            if (UI.Button("Play"))      videoPlayer.Play();
+            UI.SameLine();
+            if (UI.Button("Pause"))     videoPlayer.Pause();
+            UI.SameLine();
+            if (UI.Button("Skip -15s")) videoPlayer.Time -= 15_000;
+            UI.SameLine();
+            if (UI.Button("Skip +15s")) videoPlayer.Time += 15_000;
+
+            if (UI.HSlider("timer", ref timestampDisplay, 0, videoPlayer.Length, step: 0, notifyOn: UINotify.Finalize))
+                videoPlayer.Time = (long)timestampDisplay;
+            // Don't update the seek time if the user is interacting with it.
+            if (!UI.LastElementActive.IsActive())
+                timestampDisplay = videoPlayer.Time;
+
+            TimeSpan elapsed = TimeSpan.FromMilliseconds(videoPlayer.Time);
+            TimeSpan total   = TimeSpan.FromMilliseconds(videoPlayer.Length);
+            UI.Text( $"Time: {elapsed.ToString(@"hh\:mm\:ss")}/{total.ToString(@"hh\:mm\:ss")}" );
+
+            UI.WindowEnd();
         });
     }
 }
